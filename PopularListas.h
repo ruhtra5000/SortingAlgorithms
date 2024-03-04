5540264a75bbb99f9a4844c5fa8897775d157ce4
Arthur de Sá Tenório <60717938+ruhtra5000@users.noreply.github.com>
2024-03-03 22:38:20 -0300

    Create PopularListas.h

---
+++ PopularListas.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define MAX 10000
+
+void preencherAleatorio(int* listBubble, int* listSelection, int* listInsertion,
+    int* listQuick, int* listHeap);
+void preencherMetadeOrdenado(int* listBubble, int* listSelection,
+    int* listInsertion, int* listQuick, int* listHeap);
+void preencher75Ordenado(int* listBubble, int* listSelection,
+    int* listInsertion, int* listQuick, int* listHeap);
+void preencherOrdenado(int* listBubble, int* listSelection, int* listInsertion,
+    int* listQuick, int* listHeap);
+void preencherOrdenadoDecrescente(int* listBubble, int* listSelection, int* listInsertion, 
+    int* listQuick, int* listHeap);
